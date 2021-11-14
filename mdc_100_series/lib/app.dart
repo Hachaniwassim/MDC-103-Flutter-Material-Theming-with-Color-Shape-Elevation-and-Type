5bbe416e3fe322956b40e72f560e3b5ae1a98aa5
hachaniwassim <wassimhach16@gmail.com>
2021-11-14 18:50:26 +0100

    Creating colorsclass and  Customize ThemeData.

--- mdc_100_series/lib/app.dart
+++ mdc_100_series/lib/app.dart
@@ -16,6 +16,25 @@ import 'package:flutter/material.dart';
 
 import 'home.dart';
 import 'login.dart';
+import 'colors.dart';
+
+// TODO: Build a Shrine Theme (103)
+final ThemeData _kShrineTheme = _buildShrineTheme();
+
+ThemeData _buildShrineTheme() {
+  final ThemeData base = ThemeData.light();
+  return base.copyWith(
+    colorScheme: base.colorScheme.copyWith(
+      primary: kShrinePink100,
+      onPrimary: kShrineBrown900,
+      secondary: kShrineBrown900,
+      error: kShrineErrorRed,
+    ),
+    // TODO: Add the text themes (103)
+    // TODO: Add the icon themes (103)
+    // TODO: Decorate the inputs (103)
+  );
+}
 
 // TODO: Convert ShrineApp to stateful widget (104)
 class ShrineApp extends StatelessWidget {
@@ -25,6 +44,7 @@ class ShrineApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Shrine',
+      theme: _kShrineTheme,
       // TODO: Change home: to a Backdrop with a HomePage frontLayer (104)
       home: const HomePage(),
       // TODO: Make currentCategory field take _currentCategory (104)
@@ -34,6 +54,7 @@ class ShrineApp extends StatelessWidget {
       onGenerateRoute: _getRoute,
       // TODO: Add a theme (103)
     );
+
   }
 
   Route<dynamic>? _getRoute(RouteSettings settings) {
